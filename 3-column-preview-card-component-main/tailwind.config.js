4f523eb620a9a3a80024d4159ef855d711b66e98
Llxovell <70419875+Llxovell@users.noreply.github.com>
2022-11-26 22:21:14 -0800

    finished desktop version of component

--- 3-column-preview-card-component-main/tailwind.config.js
+++ 3-column-preview-card-component-main/tailwind.config.js
@@ -3,21 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         "bright-orange": "hsl(31, 77%, 52%)",
         "dark-cyan": "hsl(184, 100%, 22%)",
         "very-dark-cyan": "hsl(179, 100%, 13%)",
         "transparent-white": "hsla(0, 0%, 100%, 0.75)",
         "very-light-gray": "hsl(0, 0%, 95%)",
       },
-      fontSize:{
-        normal: ".9375rem"
+      fontSize: {
+        normal: ".9375rem",
       },
-      fontFamily:{
-        'bigshoulders': ('Big Shoulders Display', 'cursive'),
-        'lexend': ('Lexend Deca', 'sans-serif'),
-      }
+      fontFamily: {
+        bigshoulders: ["Big Shoulders Display", "cursive"],
+        lexend: ["Lexend Deca", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
