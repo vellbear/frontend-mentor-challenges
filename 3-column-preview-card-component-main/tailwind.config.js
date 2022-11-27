b45f569b11ab9da1ff299eb09030ec6066916fb0
Llxovell <70419875+Llxovell@users.noreply.github.com>
2022-11-26 20:57:40 -0800

    started challenge

--- 3-column-preview-card-component-main/tailwind.config.js
+++ 3-column-preview-card-component-main/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "bright-orange": "hsl(31, 77%, 52%)",
+        "dark-cyan": "hsl(184, 100%, 22%)",
+        "very-dark-cyan": "hsl(179, 100%, 13%)",
+        "transparent-white": "hsla(0, 0%, 100%, 0.75)",
+        "very-light-gray": "hsl(0, 0%, 95%)",
+      },
+      fontSize:{
+        normal: ".9375rem"
+      },
+      fontFamily:{
+        'bigshoulders': ('Big Shoulders Display', 'cursive'),
+        'lexend': ('Lexend Deca', 'sans-serif'),
+      }
+    },
   },
   plugins: [],
 }
