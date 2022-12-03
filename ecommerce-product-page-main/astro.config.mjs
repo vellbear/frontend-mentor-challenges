df5e4b3069b4c1c239cb418c1394bc5530956da8
Llxovell <70419875+Llxovell@users.noreply.github.com>
2022-12-03 11:12:31 -0800

    edited astro config

--- ecommerce-product-page-main/astro.config.mjs
+++ ecommerce-product-page-main/astro.config.mjs
@@ -9,5 +9,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://llxovell.github.io",
+  site: "https://llxovell.github.io/",
+  base: "/frontend-mentor-challenges/ecommerce-product-page-main/",
 });
